854a11f0427ae8dd89909155e126b2a357a77dab
overtho17 <tommy.overland@gmail.com>
2016-05-23 11:40:24 -0700

    Reset

---
+++ Forge/src/main/java/tealsmc/mods/items/TestItem.java
@@ -0,0 +1,15 @@
+package tealsmc.mods.items;
+
+import java.util.Set;
+
+import net.minecraft.item.ItemTool;
+
+public class TestItem extends ItemTool {
+
+	public TestItem(float arg0, ToolMaterial arg1, Set arg2) {
+		super(arg0, arg1, arg2);
+		
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Forge/src/main/java/tealsmc/mods/items/UpgradedStick.java
@@ -0,0 +1,14 @@
+package tealsmc.mods.items;
+import net.minecraft.item.Item;
+
+public class UpgradedStick extends Item {
+
+	public final Item.ToolMaterial material;
+	
+	public UpgradedStick(ToolMaterial m) {
+		// TODO Auto-generated constructor stub
+		material = m;
+		
+	}
+
+}
